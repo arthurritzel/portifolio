fb39597571db142fa4677418d078601de9c509b8
arthurritzel <ritzelarthur@gmail.com>
2023-09-21 15:05:22 -0300

    init

---
+++ script/escrever.js
@@ -0,0 +1,29 @@
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+  
+}
+const ritzel = document.querySelector('.ritzel');
+const titulo = document.querySelector('.title');
+    const textoritzel = ritzel.innerHTML.split('');
+    ritzel.innerHTML = ' ';
+typeWrite(titulo);
+
+setTimeout(()=>{
+    titulo.classList.remove("title")
+    ritzel.classList.add("title")
+    textoritzel.forEach(function(letra, i){   
+      
+        setTimeout(function(){
+            ritzel.innerHTML += letra;
+        }, 75 * i)
+    
+      });
+}, 700)
